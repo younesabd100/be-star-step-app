3905f54c2b8268e387afdbb3414a8c4c1c187af0
kozadireza <hanna.zhamoida1993@gmail.com>
2025-04-14 12:26:10 +0100

    static id were created for each document for testing DB

--- db/seed/seed.js
+++ db/seed/seed.js
@@ -5,50 +5,87 @@ const {
   tasksData,
   rewardsData,
 } = require("../test_data/index");
+const {
+  parentId,
+  kid1Id,
+  kid2Id,
+  task1Id,
+  task2Id,
+  reward1Id,
+} = require("../test_data/test.id");
+console.log(parentId, kid1Id, kid2Id, task1Id, task2Id, reward1Id);
 const { Parents, Tasks, Rewards, Kids } = require("../test_data/test.schema");
 // console.log(parentData);
 
 const seed = async () => {
   await Parents.deleteMany({}); //database keyword to delete many items/data if any exists.
-  const parentsInsertedData = await insertedParents(parentData);
+  const parentsInsertedData = await insertedParents(parentData, parentId);
 
   await Kids.deleteMany({});
-  const kidsInsertedData = await insertedKids(parentsInsertedData, kidsData); //this insert's the defined data above
+  const kidsInsertedData = await insertedKids(
+    parentsInsertedData,
+    kidsData,
+    kid1Id,
+    kid2Id
+  ); //this insert's the defined data above
 
   await Tasks.deleteMany({});
-  await insertedTasks(parentsInsertedData, kidsInsertedData, tasksData);
+  await insertedTasks(
+    parentsInsertedData,
+    kidsInsertedData,
+    tasksData,
+    task1Id,
+    task2Id
+  );
 
   await Rewards.deleteMany({});
-  await insertedRewards(parentsInsertedData, kidsInsertedData, rewardsData);
+  await insertedRewards(
+    parentsInsertedData,
+    kidsInsertedData,
+    rewardsData,
+    reward1Id
+  );
 };
 
-async function insertedParents(parentData) {
-  const insertedParentsData = await Parents.insertMany(parentData);
+async function insertedParents(parentData, parentId) {
+  const newParentData = { ...parentData, _id: parentId };
+  console.log(newParentData);
+  const insertedParentsData = await Parents.insertMany(newParentData);
   return insertedParentsData;
 }
 
-async function insertedKids(parent, kidsData) {
+async function insertedKids(parent, kidsData, kid1Id, kid2Id) {
   // console.log(parent);
   const parentID = parent[0]._id;
   const newKidsDataWithID = [];
-
+  const kidsId = [kid1Id, kid2Id];
+  let i = 0;
   kidsData.forEach((kid) => {
-    newKidsDataWithID.push({ ...kid, parentID: parentID });
+    newKidsDataWithID.push({ ...kid, parentID: parentID, _id: kidsId[i] });
+    i++;
   });
-  // console.log(newKidsDataWithID);
+  console.log(newKidsDataWithID);
   const insertedKidsData = await Kids.insertMany(newKidsDataWithID);
   return insertedKidsData;
 }
 
-async function insertedTasks(parentsData, kidsData, tasksData) {
+async function insertedTasks(
+  parentsData,
+  kidsData,
+  tasksData,
+  task1Id,
+  task2Id
+) {
   const parentID = parentsData[0]._id;
   const newTasksDataWithID = [];
+  const tasksId = [task1Id, task2Id];
   let i = 0;
   tasksData.forEach((task) => {
     newTasksDataWithID.push({
       ...task,
       createdBy: parentID,
       assignedTo: kidsData[i]._id,
+      _id: tasksId[i],
     });
     i++;
   });
@@ -57,7 +94,7 @@ async function insertedTasks(parentsData, kidsData, tasksData) {
   return insertedKidsData;
 }
 
-async function insertedRewards(parentsData, kidsData, rewardsData) {
+async function insertedRewards(parentsData, kidsData, rewardsData, reward1Id) {
   const parentID = parentsData[0]._id;
   const newRewardsDataWithID = [];
   let i = 0;
@@ -66,10 +103,11 @@ async function insertedRewards(parentsData, kidsData, rewardsData) {
       ...reward,
       createdBy: parentID,
       redeemedBy: kidsData[i]._id,
+      _id: reward1Id,
     });
     i++;
   });
-
+  console.log(newRewardsDataWithID);
   const insertedRewardsData = await Rewards.insertMany(newRewardsDataWithID);
   return insertedRewardsData;
 }
---
+++ db/test_data/test.id.js
@@ -0,0 +1,8 @@
+import { ObjectId } from "mongodb";
+
+export const parentId = new ObjectId("000000000000000000000001");
+export const kid1Id = new ObjectId("000000000000000000000002");
+export const kid2Id = new ObjectId("000000000000000000000006");
+export const task1Id = new ObjectId("000000000000000000000003");
+export const task2Id = new ObjectId("000000000000000000000004");
+export const reward1Id = new ObjectId("000000000000000000000005");
