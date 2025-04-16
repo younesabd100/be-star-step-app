ee324b14a7a4c3cb95b57d0dadc765013f8c979f
Hanna <hannaphotochester@gmail.com>
2025-04-16 22:50:36 +0100

    Fixed parent creation and validation

--- models/parents.model.js
+++ models/parents.model.js
@@ -1,5 +1,13 @@
 const { Parents } = require("../db/test_data/test.schema");
 exports.newParent = async (parentData) => {
+  
+  if (!result.parentName || !result.password) {
+    throw {
+      status: 400,
+      msg: "Missing parent name or password",
+    };
+  }
+
   const parent = await Parents.create(parentData);
   return parent;
 };
