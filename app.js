bf6b109d1c06717a6ace4a454b813b0ff69e51be
SiMercer <na>
2025-04-19 20:25:30 +0100

    verison route added

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const apiRouter = require("./Routes/api.router");
 const cors = require("cors");
 const { connectDB } = require("./db/connection");
 const userRoutes = require("./Routes/user");
+const versionRouter = require("./Routes/version");
 
 app.use(cors());
 connectDB();
@@ -13,7 +14,6 @@ app.use(express.json());
 app.use("/api", apiRouter);
 app.use("/api/user", require("./Routes/user"));
 
-const versionRouter = require("./routes/version");
 app.use("/api/version", versionRouter);
 
 // app.all("*", (req, res) => {
