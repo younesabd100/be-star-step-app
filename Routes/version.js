cc2d82fd2e624657ca507b384aaf2f0088a45bad
SiMercer <na>
2025-04-19 21:32:08 +0100

    verison route added

--- Routes/version.js
+++ Routes/version.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.status(200).send({ version: "1.0.3 - Updated on 18 April 2025" });
+  res.status(200).send({ version: "1.0.3 - Updated on 19 April 2025" });
 });
 
 module.exports = router;
