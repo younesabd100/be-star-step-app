a7a125dae54f96e53d9789178a2780464c520593
Younes <younes.abderral@gmail.com>
2025-04-22 12:18:34 +0100

    updtating route

--- Routes/kids.js
+++ Routes/kids.js
@@ -6,7 +6,6 @@ const {
 } = require("../controllers/kids_controller");
 
 const kidRoutes = require("express").Router();
-kidRoutes.route("/").post(postKid);
-kidRoutes.route("/:parentID").get(getKidByParentId);
+kidRoutes.route("/").post(postKid).get(getKidByParentId);
 kidRoutes.route("/:childId").get(getKidById).patch(patchStarsKidById);
 module.exports = kidRoutes;
